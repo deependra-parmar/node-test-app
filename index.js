e4e998c16afb121845c2c512de8bbbe8d4529ed1
deependra-parmar <dparmar@qodeleaf.com>
2025-03-20 12:50:23 +0530

    Added Dockerfile

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Server running fine"
+    });
+})
+
+app.get("/foo", (req, res) => {
+    res.json({
+        success: true,
+        foo: "Hey there, I am foo and I hope you are good"
+    })
+})
+
+app.get("/bar", (req, res) => {
+    res.json({
+        success: true,
+        foo: "Hey there, I am bar and I hope you are good"
+    })
+})
+
+app.listen(4000, () => {
+    console.log(`Server running at port 4000`);
+})
